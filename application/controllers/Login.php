5e1f2e3b31708ec8c9e41011223a53556fbdd48e
Francisco Geneuto <geneuto@gmail.com>
2020-09-29 15:15:36 -0300

    Adicionado o construtor no controller

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,10 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
+	function __construct()
+    {
+        parent::__construct();
+    }
 
 	public function index()
 	{
 		$dados['title_page'] = "Aroma Spot - Login";
+
 		$this->load->view('app/paginas/login', $dados);
 	}
 }
