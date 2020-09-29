20d11230cf32012e122bdc8a484e977e02c7fbe7
Francisco Geneuto <geneuto@gmail.com>
2020-09-29 16:22:49 -0300

    Ajuste da base url

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -9,8 +9,20 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
+		// Verifica se o usuário esta logado
+        if ((!session_id()) || ($this->session->userdata('logado'))) {
+            redirect('dashboard');
+		}
+		
 		$dados['title_page'] = "Aroma Spot - Login";
 
-		$this->load->view('app/paginas/login', $dados, TRUE);
+		$this->load->view('app/paginas/login', $dados);
 	}
+
+	// Função de sair
+    public function sair()
+    {
+        $this->session->sess_destroy();
+        redirect(base_url() . 'login');
+    }
 }
--- application/views/app/paginas/login.php
+++ application/views/app/paginas/login.php
@@ -1,7 +1,7 @@
 <?php
     // CONTEUDO DA PAGINA
-    echo $this->load->view('app/fragmentos/login/index');
+    $this->load->view('app/fragmentos/login/index');
 
     // RODAPE DA PRINCIPAL
-    //echo $this->load->view('app/estrutura/footer'); 
+    //$this->load->view('app/estrutura/footer'); 
 ?>
