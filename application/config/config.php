101366a3b84cf6621fa98ded54cbdd3523781d08
Francisco Geneuto <geneuto@gmail.com>
2020-10-01 13:48:32 -0300

    API ativa

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://www.syspro.club/aromaspot/';
+$config['base_url'] = 'https://www.syspro.club/aromni/';
 //$config['base_url'] = '';
 /*
 |--------------------------------------------------------------------------
@@ -376,7 +376,7 @@ $config['encryption_key'] = 'ddb99b24fac9fe8';
 | Other session cookie settings are shared with the rest of the application,
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
-*/
+
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
@@ -384,7 +384,15 @@ $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+*/
 
+$config['sess_driver'] = 'database';
+$config['sess_cookie_name'] = 'app_session';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = 'ci_sessions';
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
