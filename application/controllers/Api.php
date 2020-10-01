294f476d33d8f754b134e8e34ba3236741de10d8
Francisco Geneuto <geneuto@gmail.com>
2020-10-01 13:37:55 -0300

    API ativa

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -17,22 +17,61 @@ class Api extends CI_Controller
         $this->load->library('upload');
     }
 
-    private function Eugenio_API_Config($parr)
+    public function teste()
     {
-        $ApiKey = "1J0Uip9R4agRfEg0SZjAGV1VXVsNzpW6";
-        $url = "https://portal.stg.eugenio.io/api/v1/";
+        $this->load->view('app/estrutura/request');
+    }
 
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    private function Eugenio_Credenciais()
+    {
+        $api = array(
+            'apikey: 1J0Uip9R4agRfEg0SZjAGV1VXVsNzpW6', //Key da API do Eugenio
+            'Content-Type: application/json',
+        );
 
-        $resultado = json_decode(curl_exec($ch));
+        return $api;
+    }
 
+    private function Eugenio_Connect($metodo, $url, $data, $api){
+        $curl = curl_init();
+        switch ($metodo){
+           case "POST":
+              curl_setopt($curl, CURLOPT_POST, 1);
+              if ($data)
+                 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+              break;
+           case "PUT":
+              curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+              if ($data)
+                 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+              break;
+           default:
+              if ($data)
+                 $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        // OPÇÕES:
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $api);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+        // EXECUÇÃO:
+        $resultado = curl_exec($curl);
+        if(!$resultado){die("A conexão falhou");}
+        curl_close($curl);
+
+        return $resultado;
     }
 
     public function Eugenio_Devices_Return()
     {
-        $local_lib = "apis/dataquery";
+        $api = $this->Eugenio_Credenciais();
+        $device_id = "639d1942-6dab-45f1-aede-a3ac6d91a83e";
+        $endpoint = 'things/'.$device_id.'/invoke';
+        $url = "https://portal.stg.eugenio.io/api/v1/" . $endpoint;
+        $data = [];
+
+        echo "<pre>".print_r($this->Eugenio_Connect("GET", $url, $data, $api))."</pre>";
     }
 }
